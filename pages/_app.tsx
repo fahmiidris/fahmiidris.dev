11fb5ebeb5e37fa37d66dd9f1299c7b0b6d7115f
Fahmi Idris <fahmiidris.1607@gmail.com>
2021-10-06 10:24:29 +0700

    Format and delete pages api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <Component {...pageProps} />
+    )
 }
+
 export default MyApp
