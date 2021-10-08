acf63e9b35b2dfa79bcf58b0a9f1f5eb5bb546b2
Fahmi Idris <fahmiidris.1607@gmail.com>
2021-10-08 22:50:53 +0700

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document"
 
+import Favicon from "@/components/custom/Favicon"
+
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx)
@@ -12,6 +14,7 @@ class MyDocument extends Document {
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+                    <Favicon />
                 </Head>
                 <body className="antialiased font-sans bg-white dark:bg-gray-700">
                     <Main />
