bb47c7a01dbb45b7cd2d54ab40d6a6d956908f98
Fahmi Idris <fahmiidris.1607@gmail.com>
2021-10-17 21:40:01 +0700

    Change: change heroicons to react-icons

--- components/small/ThemeSwitcherDesktop.tsx
+++ components/small/ThemeSwitcherDesktop.tsx
@@ -1,8 +1,7 @@
-import { MoonIcon, SunIcon } from "@heroicons/react/outline"
 import { useTheme } from "next-themes"
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi"
 
-const ThemeSwitcherDesktop: React.FC = () => {
-
+export const ThemeSwitcherDesktop: React.FC = () => {
     const { theme, setTheme } = useTheme()
     const isDark = theme === "dark"
 
@@ -10,9 +9,9 @@ const ThemeSwitcherDesktop: React.FC = () => {
         <button type="button" onClick={() => setTheme(isDark ? "light" : "dark")} className="inline-flex justify-center items-center text-white bg-gray-600 dark:bg-gray-700 px-2 py-2 rounded-lg text-sm font-semibold hover:bg-gray-800 dark:hover:bg-gray-900 duration-100 ease-in-out">
             <span className="sr-only">Dark or Light</span>
             {!isDark ? (
-                <SunIcon className="w-5 h-5" />
+                <HiOutlineSun className="w-5 h-5" />
             ) : (
-                <MoonIcon className="w-5 h-5" />
+                <HiOutlineMoon className="w-5 h-5" />
             )}
         </button>
     )
--- components/small/ThemeSwitcherMobile.tsx
+++ components/small/ThemeSwitcherMobile.tsx
@@ -1,8 +1,7 @@
-import { MoonIcon, SunIcon } from "@heroicons/react/outline"
 import { useTheme } from "next-themes"
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi"
 
 const ThemeSwitcherMobile: React.FC = () => {
-
     const { theme, setTheme } = useTheme()
     const isDark = theme === "dark"
 
@@ -10,9 +9,9 @@ const ThemeSwitcherMobile: React.FC = () => {
         <button type="button" onClick={() => setTheme(isDark ? "light" : "dark")} className="inline-flex items-center justify-center p-2 rounded-lg text-white bg-gray-600 dark:bg-gray-700 hover:bg-gray-800 dark:hover:bg-gray-900 duration-150 ease-in-out">
             <span className="sr-only">Dark or Light</span>
             {!isDark ? (
-                <SunIcon className="block h-6 w-6" aria-hidden="true" />
+                <HiOutlineSun className="block h-6 w-6" aria-hidden="true" />
             ) : (
-                <MoonIcon className="block h-6 w-6" aria-hidden="true" />
+                <HiOutlineMoon className="block h-6 w-6" aria-hidden="true" />
             )}
         </button>
     )
