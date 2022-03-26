3fafd95b77149e3c74da30244d702d6c7765c585
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-03-26 19:13:14 +0700

    style: change responsive class from md: to lg:

--- src/components/organisms/fi-navbar/fi-switch-theme/switch-theme.tsx
+++ src/components/organisms/fi-navbar/fi-switch-theme/switch-theme.tsx
@@ -10,13 +10,13 @@ export const SwitchTheme = () => {
     <button
       type="button"
       onClick={() => setTheme(isDark ? 'light' : 'dark')}
-      className="group rounded-lg border border-gray-100 bg-white p-2 text-gray-400 duration-200 ease-in-out hover:bg-gray-100 md:text-gray-700"
+      className="group rounded-lg border border-gray-100 bg-white p-2 text-gray-400 duration-200 ease-in-out hover:bg-gray-100 lg:text-gray-700"
     >
       <span className="sr-only">Dark or Light</span>
       {!isDark ? (
-        <HiOutlineSun className="h-6 w-6 group-hover:animate-spin-slow md:h-5 md:w-5" />
+        <HiOutlineSun className="h-6 w-6 group-hover:animate-spin-slow lg:h-5 lg:w-5" />
       ) : (
-        <HiOutlineMoon className="h-6 w-6 group-hover:animate-wiggle md:h-5 md:w-5" />
+        <HiOutlineMoon className="h-6 w-6 group-hover:animate-wiggle lg:h-5 lg:w-5" />
       )}
     </button>
   );
