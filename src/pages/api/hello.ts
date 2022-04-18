456748e181f7bef19c7c61532b9b5add77b732e1
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-18 21:24:33 +0700

    feat: add address to response data

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,9 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 type TData = {
   name: string;
+  address: string;
   age: number;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<TData>) {
-  res.status(200).json({ name: 'Mamang John Doe', age: 20 });
+export default function handler(req: NextApiRequest, res: NextApiResponse<TData>): void {
+  res.status(200).json({ name: 'Mamang John Doe', address: 'West Java - Indonesia', age: 20 });
 }
