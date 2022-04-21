c3ae050006fa028d7a3fcfae0e8b7b6a112510fd
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-21 20:56:56 +0700

    feat: add class when dark mode

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -14,17 +14,17 @@ const Custom500Page: TNextPageWithLayout = (): JSX.Element => {
           <Logo className="mx-auto h-10 w-auto" />
         </Link>
         <div className="pb-4">
-          <h1 className="text-center text-2xl font-extrabold text-slate-800">
+          <h1 className="text-center text-2xl font-extrabold text-slate-800 dark:text-slate-200">
             500 Server-side error occurred!
           </h1>
-          <p className="text-center text-sm text-slate-600">
+          <p className="text-center text-sm text-slate-600 dark:text-slate-500">
             Opps! Sorry, There seems to be an error on the server.
           </p>
         </div>
         <div className="relative">
           <Link
             href="/"
-            className="inline-flex w-full items-center justify-center rounded-lg bg-slate-700 py-3 text-sm font-semibold text-white duration-200 ease-in-out hover:bg-slate-800 hover:ring-2 hover:ring-cyan-400 hover:ring-offset-2"
+            className="inline-flex w-full items-center justify-center rounded-lg bg-slate-700 py-3 text-sm font-semibold text-white duration-200 ease-in-out hover:bg-slate-800 hover:ring-2 hover:ring-cyan-400 hover:ring-offset-2 dark:bg-slate-800 dark:hover:bg-slate-700 dark:hover:ring-offset-slate-900"
           >
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <ArrowCircleLeftIcon className="h-5 w-5" />
