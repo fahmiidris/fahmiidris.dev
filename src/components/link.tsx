b55f2c1d14b54b71f1abd7e70134c40e1c1c688b
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-22 22:20:32 +0700

    refactor: add forwardRef to link component and disable eslint rule display name

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -10,31 +10,34 @@ export type TLink = {
 } & React.ComponentPropsWithoutRef<'a'> &
   LinkProps;
 
-export const Link = ({ children, href, openNewTab, className, ...props }: TLink): JSX.Element => {
-  const isNewTab: boolean = openNewTab
-    ? openNewTab
-    : !href.startsWith('/') && !href.startsWith('#');
+export const Link = React.forwardRef<HTMLAnchorElement, TLink>(
+  ({ children, href, openNewTab, className, ...props }, ref): JSX.Element => {
+    const isNewTab: boolean = openNewTab
+      ? openNewTab
+      : !href.startsWith('/') && !href.startsWith('#');
 
-  if (!isNewTab) {
+    if (!isNewTab) {
+      return (
+        <NextLink href={href}>
+          <a {...props} ref={ref} className={className} style={{ outline: 'none' }}>
+            {children}
+          </a>
+        </NextLink>
+      );
+    }
+
     return (
-      <NextLink href={href}>
-        <a {...props} className={className} style={{ outline: 'none' }}>
-          {children}
-        </a>
-      </NextLink>
+      <a
+        href={href}
+        ref={ref}
+        className={clsx(className, 'cursor-pointer')}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ outline: 'none' }}
+        {...props}
+      >
+        {children}
+      </a>
     );
   }
-
-  return (
-    <a
-      href={href}
-      className={clsx(className, 'cursor-pointer')}
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ outline: 'none' }}
-      {...props}
-    >
-      {children}
-    </a>
-  );
-};
+);
