aba08490286dd0e5f3a2778b39ffe2468a8e0739
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-24 23:52:54 +0700

    style: re-styling no-content component

--- src/components/home/no-content.tsx
+++ src/components/home/no-content.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export const NoContent = (): JSX.Element => {
   return (
-    <div className="flex h-32 w-full items-center justify-center rounded-lg border border-dashed border-slate-900/30 dark:border-slate-50/[0.06]">
+    <div className="block w-full border border-dashed border-slate-700 py-2 px-4 text-sm">
       <p>No Content Here!</p>
     </div>
   );
