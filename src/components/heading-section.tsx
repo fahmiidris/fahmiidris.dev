1edcac8dcb16904c82e30eb975f180ba2ec84df3
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-24 20:36:06 +0700

    fix: inconsistent icon size in mobile mode.

--- src/components/heading-section.tsx
+++ src/components/heading-section.tsx
@@ -9,10 +9,12 @@ type THeadingSection = {
 export const HeadingSection = ({ title, description, icon }: THeadingSection): JSX.Element => {
   return (
     <div className="flex items-center justify-start">
-      <div className="col-span-9 flex items-start justify-start">
-        {React.createElement(icon, {
-          className: 'w-8 h-8 -ml-1 mr-2 stroke-slate-700 dark:stroke-slate-200',
-        })}
+      <div className="flex items-start justify-start">
+        <div className="mr-2">
+          {React.createElement(icon, {
+            className: 'w-8 h-8 stroke-slate-700 dark:stroke-slate-200',
+          })}
+        </div>
         <div className="flex flex-col">
           <h2 className="text-lg font-semibold text-slate-700 dark:text-slate-200">{title}</h2>
           <p className="text-xs font-semibold text-slate-500 dark:text-slate-500">{description}</p>
