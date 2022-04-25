3bbccb0b7a1347250e834d6190ba823b14b8c8ab
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-25 12:40:34 +0700

    feat: add some experiences and steps component

--- src/components/home/latest-experience.tsx
+++ src/components/home/latest-experience.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import { ClockIcon } from '@heroicons/react/outline';
 
+import { Link } from '@/components/link';
+import { Steps } from '@/components/experiences/steps';
 import { NoContent } from '@/components/home/no-content';
 import { HeadingSection } from '@/components/heading-section';
+
+import { steps } from '@/pages/experiences';
 
 export const LatestExperience = (): JSX.Element => {
   return (
@@ -15,7 +19,21 @@ export const LatestExperience = (): JSX.Element => {
         />
       </div>
       <div className="container pt-8">
-        <NoContent />
+        {steps.length < 1 ? (
+          <NoContent />
+        ) : (
+          <div className="flex flex-col space-y-8">
+            <Steps steps={steps.slice(0, 2)} />
+            <div className="ml-10">
+              <Link
+                href="/experiences"
+                className="inline-flex items-center space-x-2 rounded-lg border border-slate-300 bg-slate-100 py-2 px-4 text-sm font-semibold text-slate-700 hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 dark:border-transparent dark:bg-slate-800 dark:text-slate-300 dark:ring-offset-slate-900 dark:hover:bg-slate-700"
+              >
+                View More
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
