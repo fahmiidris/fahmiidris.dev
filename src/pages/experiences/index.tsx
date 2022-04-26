429aa00dd0fe525ef825ef15d6073dd73e4ae663
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-26 13:31:34 +0700

    feat: add search for my experiences

--- src/pages/experiences/index.tsx
+++ src/pages/experiences/index.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react';
-import { ClockIcon } from '@heroicons/react/outline';
+import { ClockIcon, SearchIcon } from '@heroicons/react/outline';
 
-import { Steps } from '@/components/experiences/steps';
+import { Steps, TSteps } from '@/components/experiences/steps';
 import { BasicLayout } from '@/layouts/basic-layout';
 import { HeadingSection } from '@/components/heading-section';
 
 import { data as myExperiences } from '@/components/experiences/data';
 
 import type { TNextPageWithLayout } from 'next';
+import { Label } from '@/components/label';
 
 const ExperiencesPage: TNextPageWithLayout = (): JSX.Element => {
+  const [search, setSearch] = React.useState<string | null>(null);
+  const [data, setData] = React.useState<TSteps['steps']>(myExperiences);
+
+  React.useEffect(() => {
+    if (search) {
+      const filteredData: TSteps['steps'] = myExperiences
+        .sort((a, b) => b.id - a.id)
+        .filter((step) => {
+          return step.title.toLowerCase().includes(search.toLowerCase());
+        });
+
+      setData(filteredData);
+    } else {
+      setData(myExperiences);
+    }
+  }, [search]);
+
   return (
     <>
       <div className="relative mb-20">
@@ -20,11 +38,28 @@ const ExperiencesPage: TNextPageWithLayout = (): JSX.Element => {
             icon={ClockIcon}
           />
         </div>
-        <div className="container min-h-screen pt-8">
+        <div className="container pt-8">
           <div className="grid grid-cols-12 gap-4">
-            <div className="col-span-12 md:col-span-3">{/*  */}</div>
-            <div className="col-span-12 md:col-span-9">
-              <Steps steps={myExperiences.sort((a, b) => b.id - a.id)} />
+            <div className="sticky top-20 col-span-12 self-start md:col-span-3">
+              <div>
+                <Label htmlFor="search" value="Search" />
+                <div className="relative mt-1">
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <SearchIcon className="h-[18px] w-[18px]" />
+                  </span>
+                  <input
+                    type="text"
+                    name="search"
+                    placeholder='Search "My Experiences"'
+                    className="block w-full rounded-lg border border-slate-300/50 pl-9 text-xs placeholder:text-xs placeholder:font-semibold focus:border-slate-300/75 focus:ring-2 focus:ring-cyan-400 dark:border-transparent dark:bg-slate-800 dark:ring-offset-slate-900"
+                    style={{ outline: 'none' }}
+                    onChange={(e) => setSearch(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-span-12 pt-4 md:col-span-9 md:pt-0">
+              <Steps steps={data} />
             </div>
           </div>
         </div>
