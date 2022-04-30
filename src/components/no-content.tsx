a845b8a782f39969f8f96df1729e0ec8928bf361
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-04-30 23:39:30 +0700

    feat: add className parameter

--- src/components/no-content.tsx
+++ src/components/no-content.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
+import clsx from 'clsx';
 
 type TNoContent = {
   text: string;
+  className?: string;
   children?: React.ReactNode;
 };
 
-export const NoContent = ({ text, children }: TNoContent): JSX.Element => {
+export const NoContent = ({ text, className, children }: TNoContent): JSX.Element => {
   return (
-    <div className="block w-full border border-dashed border-slate-700 py-2 px-4 text-sm">
+    <div
+      className={clsx(
+        'block w-full border border-dashed border-slate-700 py-2 px-4 text-sm',
+        className
+      )}
+    >
       <p>{text ? text : children}</p>
     </div>
   );
