034e85221ec6127ca7590f8ca7baec20f0c93b7b
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-05-05 10:47:53 +0700

    feat: add tech category and link to github and to view website

--- src/components/projects/data.tsx
+++ src/components/projects/data.tsx
@@ -1,10 +1,61 @@
 import * as React from 'react';
 
+type TTech = {
+  title: string;
+  href: string;
+  className?: string;
+};
+
 export type TProject = {
   id: number;
   title: string;
   description: string;
+  href: string;
   image?: any;
+  github?: string;
+  tech?: TTech[];
+};
+
+const javascript: TTech = {
+  title: 'JavaScript',
+  href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  className: 'text-slate-700 bg-yellow-400',
+};
+
+const typescript: TTech = {
+  title: 'TypeScript',
+  href: 'https://www.typescriptlang.org/',
+  className: 'text-white bg-sky-500',
+};
+
+const reactjs: TTech = {
+  title: 'React.js',
+  href: 'https://reactjs.org/',
+  className: 'text-white bg-cyan-500',
+};
+
+const nextjs: TTech = {
+  title: 'Next.js',
+  href: 'https://nextjs.org/',
+  className: 'text-slate-700 bg-slate-200',
+};
+
+const vuejs: TTech = {
+  title: 'Vue.js',
+  href: 'https://vuejs.org/',
+  className: 'text-white bg-emerald-500',
+};
+
+const nuxtjs: TTech = {
+  title: 'Nuxt.js',
+  href: 'https://nuxtjs.org/',
+  className: 'text-white bg-emerald-500',
+};
+
+const tailwindcss: TTech = {
+  title: 'Tailwind CSS',
+  href: 'https://tailwindcss.com/',
+  className: 'text-slate-700 bg-cyan-400',
 };
 
 export const kloningan: TProject[] = [];
@@ -14,29 +65,43 @@ export const random: TProject[] = [
     id: 1,
     title: 'Todo List Management',
     description: 'Todo List Management for Manage Your Task!',
+    href: 'https://todo.fahmiidris.dev',
+    github: 'https://www.github.com/fahmiidris-labs/todo.fahmiidris.dev',
+    tech: [typescript, vuejs, tailwindcss],
   },
   {
     id: 2,
     title: 'Movies and Games Rating',
     description: 'Website That Show Ratings Popular Movies and Games!',
+    href: '#',
     image: require('@/images/projects/movies-and-games-rating.jpg').default,
+    github: 'https://github.com/fahmiidris-labs/React-JS-JCC-2021/tree/main/Final-Project',
+    tech: [javascript, reactjs, tailwindcss],
   },
   {
     id: 3,
     title: 'Bansos Ku',
     description: 'Only Form Validation Level Frontend Using Formik!',
+    href: '#',
     image: require('@/images/projects/bansosku.jpg').default,
+    github: 'https://www.github.com/fahmiidris-labs/bansosku',
+    tech: [typescript, nextjs, tailwindcss],
   },
   {
     id: 4,
     title: 'Clover Web App',
     description: 'Embrance New Ways to Farm!',
+    href: 'https://clover-app.vercel.app',
     image: require('@/images/projects/clover.jpg').default,
+    tech: [javascript, nextjs, tailwindcss],
   },
   {
     id: 5,
     title: 'Buku Angkatan JCC Offline 2021',
     description: 'Tinggal di Desa, Rezeki Kota, Skill Mendunia!',
+    href: '#',
     image: require('@/images/projects/class-book.jpg').default,
+    github: 'https://github.com/fahmiidris-labs/buku-angkatan-jcc-2021',
+    tech: [typescript, nextjs, tailwindcss],
   },
 ];
--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import clsx from 'clsx';
 
+import { Link } from '@/components/link';
 import { Image } from '@/components/image';
 
 import type { TProject } from '@/components/projects/data';
@@ -15,10 +16,12 @@ export const ProjectCard = ({
   description,
   image,
   className,
+  github,
+  tech,
 }: TProjectCard): JSX.Element => {
   return (
     <div className={clsx('flex flex-col space-y-2', className)}>
-      <Image.Wrapper className="relative col-span-3 aspect-video w-full overflow-hidden rounded-lg border border-slate-200 bg-slate-200 dark:border-slate-800 dark:bg-slate-800 sm:col-span-1">
+      <Image.Wrapper className="group relative col-span-3 aspect-video w-full overflow-hidden rounded-lg border border-slate-200 bg-slate-200 hover:shadow dark:border-slate-800 dark:bg-slate-800 sm:col-span-1">
         <Image
           src={image ? image : require('@/images/projects/default-cover.jpg').default}
           alt={title}
@@ -26,6 +29,38 @@ export const ProjectCard = ({
           placeholder="blur"
           className="object-cover object-center"
         />
+        <div className="absolute right-2 top-2 flex items-center space-x-2 lg:-translate-y-8 lg:transition-transform lg:duration-500 lg:group-hover:translate-y-0">
+          {github && (
+            <Link
+              href={github}
+              className="inline-flex items-center justify-center rounded-[4px] bg-slate-800 px-1.5 py-0.5 text-[10px] font-bold uppercase text-white"
+            >
+              GitHub
+            </Link>
+          )}
+          <Link
+            href=""
+            className="inline-flex items-center justify-center rounded-[4px] bg-slate-200 px-1 py-0.5 text-[10px] font-bold uppercase text-slate-700"
+          >
+            View Website
+          </Link>
+        </div>
+        {tech && (
+          <div className="absolute bottom-2 left-2 flex items-center space-x-2">
+            {tech.map((item, idx) => (
+              <Link
+                key={idx}
+                href={item.href}
+                className={clsx(
+                  'inline-flex items-center justify-center rounded-[4px]  px-1.5 py-0.5 text-[10px] font-bold uppercase',
+                  item.className ? item.className : 'bg-cyan-400 text-slate-700'
+                )}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </Image.Wrapper>
       <div className="flex flex-col">
         <h3 className="text-lg font-semibold text-slate-800 dark:text-slate-200">{title}</h3>
