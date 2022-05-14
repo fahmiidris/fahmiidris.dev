b3efa1b9d239c0baede5a81ea682b68b4f848aea
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-05-14 23:19:50 +0700

    feat: update data body internship at efishery

--- src/components/experiences/data.tsx
+++ src/components/experiences/data.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ExternalLinkIcon } from '@heroicons/react/outline';
 
 import { Link } from '@/components/link';
 
@@ -33,7 +34,7 @@ export let data: TExperience[] = [
         <Link
           href="https://drive.google.com/file/d/1W3BUSYCBfrLVM0lSlpJSzZVi5_-RVakE/view?usp=sharing"
           openNewTab={true}
-          className="inline-flex items-center rounded-md bg-cyan-400 px-4 py-2 text-xs text-white no-underline hover:bg-cyan-500 dark:bg-cyan-500 dark:hover:bg-cyan-600"
+          className="inline-flex items-center rounded-md border border-transparent bg-cyan-400 px-4 py-2 text-xs text-white no-underline hover:bg-cyan-500 dark:bg-cyan-500 dark:hover:bg-cyan-600"
         >
           View Certificate
         </Link>
@@ -59,7 +60,7 @@ export let data: TExperience[] = [
         <Link
           href="https://drive.google.com/file/d/1whSsmdfx18tIxbacbXdrVb8sFdOO2fsw/view?usp=sharing"
           openNewTab={true}
-          className="inline-flex items-center rounded-md bg-cyan-400 px-4 py-2 text-xs text-white no-underline hover:bg-cyan-500 dark:bg-cyan-500 dark:hover:bg-cyan-600"
+          className="inline-flex items-center rounded-md border border-transparent bg-cyan-400 px-4 py-2 text-xs text-white no-underline hover:bg-cyan-500 dark:bg-cyan-500 dark:hover:bg-cyan-600"
         >
           View e-Certificate
         </Link>
@@ -116,6 +117,48 @@ export let data: TExperience[] = [
         <p>
           Internship at eFishery to be precise in the Feeder Ops section as a Frontend Engineer.
         </p>
+        <div className="pb-5">
+          <h3 className="text-sm text-slate-800 dark:text-slate-200">My Graduation Presentation</h3>
+          <div className="flex items-center justify-start space-x-2">
+            <div className="flex items-center space-x-1">
+              <Link
+                href="https://drive.google.com/file/d/1NpheHjd3l8UG1jYLn_nXm1D-mZBwODG7/view?usp=sharing"
+                className="animated-underline text-xs font-semibold text-slate-800 no-underline dark:text-slate-200"
+                openNewTab={true}
+              >
+                PDF Version
+              </Link>
+              <ExternalLinkIcon className="h-[16px] w-[16px] text-slate-800 dark:text-slate-200" />
+            </div>
+            <span className="pr-[2px] text-xs font-semibold dark:text-slate-400">or</span>
+            <div className="flex items-center space-x-1">
+              <Link
+                href="https://graduation-efishery.fahmiidris.dev"
+                className="animated-underline text-xs font-semibold text-slate-800 no-underline dark:text-slate-200"
+                openNewTab={true}
+              >
+                Web Version
+              </Link>
+              <ExternalLinkIcon className="h-[16px] w-[16px] text-slate-800 dark:text-slate-200" />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          {/* <Link
+            href="#"
+            openNewTab={true}
+            className="inline-flex items-center rounded-md border border-transparent bg-cyan-400 px-4 py-2 text-xs text-white no-underline hover:bg-cyan-500 dark:bg-cyan-500 dark:hover:bg-cyan-600"
+          >
+            View e-Certificate
+          </Link> */}
+          <Link
+            href="https://docs.google.com/document/d/1mCpq6G0aqdBvi3nQsi68VlY6Pp-PwI4zgSGj7F3t0sY/edit?usp=sharing"
+            openNewTab={true}
+            className="inline-flex items-center rounded-md border border-slate-400/20 bg-transparent px-4 py-2 text-xs text-slate-700 no-underline hover:bg-slate-100 dark:text-white dark:hover:bg-slate-800"
+          >
+            Feedback for Me
+          </Link>
+        </div>
       </>
     ),
   },
