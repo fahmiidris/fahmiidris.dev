1f056a19ae8fb68e4290f77eed3165439c8465f7
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-06-25 10:52:05 +0700

    refactor(blog): add responsive search form on preview blog

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -28,14 +28,14 @@ export const Input = ({
     }, [isFocused]);
 
     return (
-        <div className="group relative flex flex-col items-start">
+        <div className="group relative flex flex-col items-start w-full">
             {icon && React.createElement(icon, { className: "pointer-events-none absolute inset-y-0 left-2.5 h-full w-6 text-slate-400 group-focus-within:text-cyan-500 dark:group-focus-within:text-slate-400" })}
             <input
                 type={type}
                 inputMode={inputMode}
                 name={name}
                 value={value}
-                className={clsx("block w-full appearance-none rounded-md border !border-transparent bg-white py-2 pr-3 text-sm leading-5 text-slate-900 ring-1 ring-slate-900/20 placeholder:text-slate-400 focus:ring-2 focus:ring-cyan-500 dark:bg-slate-800/20 dark:text-white dark:ring-slate-200/20 dark:focus:ring-cyan-500", { "pl-11": !!icon } ,className)}
+                className={clsx("block w-full appearance-none rounded-md border !border-transparent bg-white py-2 pr-3 text-sm leading-5 text-slate-900 ring-1 ring-slate-900/20 placeholder:text-slate-400 focus:ring-2 focus:ring-cyan-500 dark:bg-slate-800/20 dark:text-white dark:ring-slate-200/20 dark:focus:ring-cyan-500", { "pl-11": !!icon }, className)}
                 placeholder={placeholder}
                 ref={input}
                 autoComplete={autoComplete}
