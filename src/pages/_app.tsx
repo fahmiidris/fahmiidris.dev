e3200b1e06d5eac639fd24b5db333be7b0223c96
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-08-22 22:22:32 +0700

    feat: add default seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,10 @@ import '@/css/fonts.css';
 import '@/css/main.css';
 
 import * as React from 'react';
+import Head from 'next/head';
 import Router from 'next/router';
+
+import { Title } from '@/components/title';
 
 import ProgressBar from '@/utils/progress-bar';
 
@@ -24,8 +27,47 @@ Router.events.on('routeChangeStart', () => progress.start());
 Router.events.on('routeChangeComplete', () => progress.finish());
 Router.events.on('routeChangeError', () => progress.finish());
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const defaultMeta = {
+  title: "Hi, I'm Fahmi Idris",
+  description: 'Fahmi Idris Personal Portfolio Website, Blog, Project Showcase, and My Experience History.',
+  url: 'https://www.fahmiidris.dev',
+  image: 'https://www.fahmiidris.dev/images/default-og.png',
+  type: 'website',
+  robots: 'follow, index',
+};
+
+const MyApp = ({ Component, pageProps, router }: AppProps) => {
+  const meta = Object.assign({}, defaultMeta);
+
+  return (
+    <>
+      <Title suffix="Fahmi Idris Portfolio">{meta.title}</Title>
+
+      <Head>
+        <meta name="robots" content={meta.robots} />
+        <meta name="description" content={meta.description} />
+        <meta name="keywords" content="fahmi,idris,fahmi idris,fahmiidris" />
+
+        {/* Open Graph */}
+        <meta name="type" property="og:type" content={meta.type} />
+        <meta name="image" property="og:image" content={meta.image} />
+        <meta name="site_name" property="og:site_name" content={meta.title} />
+        <meta name="url" property="og:url" content={`${meta.url}${router.pathname}`} />
+        <meta name="description" property="og:description" content={meta.description} />
+
+        {/* Twitter */}
+        <meta name="twitter:image" content={meta.image} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:site" content="@fahmiidris_dev" />
+        <meta name="twitter:creator" content="@fahmiidris_dev" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <link rel="canonical" href={`${meta.url}${router.pathname}`} />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
