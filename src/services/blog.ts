eb4439555229d53484ec0c6cbaa23d2c915b8b9d
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-09-04 21:42:26 +0700

    feat: create mdx service

---
+++ src/services/blog.ts
@@ -0,0 +1,17 @@
+import { sortDateDesc } from '@/utils/helpers';
+
+import type { Blog } from '@/types/blog';
+
+export const getBlogPreviews = () => {
+  const context = require.context(`../pages/blog/?preview`, true, /\.mdx$/);
+
+  return context
+    .keys()
+    .filter((file) => file.startsWith('.'))
+    .map((file) => ({
+      slug: file.substring(2).replace(/\/index\.mdx$/, ''),
+      module: context<Blog.Module>(file),
+    }))
+    .filter((p) => p.module.meta.private !== true)
+    .sort((a, b) => sortDateDesc(a.module.meta.createdAt, b.module.meta.createdAt));
+};
