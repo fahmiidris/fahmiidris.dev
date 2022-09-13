dd00176d5f60fc870382e9df6b883d461802eb5a
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-09-13 14:31:39 +0700

    feat: add truncate to announcement message

--- src/components/navbar/announcement.tsx
+++ src/components/navbar/announcement.tsx
@@ -16,7 +16,7 @@ export const Announcement = ({ href, message }: AnnouncementProps) => {
       className="group block bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 p-3"
     >
       <p className="flex items-center justify-center text-xs font-medium text-white transition group-hover:text-opacity-75">
-        {message}
+        <span className="truncate">{message}</span>
         <SparklesIcon className="ml-1.5 h-4 w-4" />
       </p>
     </Link>
