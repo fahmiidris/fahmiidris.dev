0d759e1b91489c7aacb9a863c7df797100a0bc4d
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-09-14 18:54:45 +0700

    style: add whitespace

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -34,13 +34,16 @@ export const Header = () => {
           className="absolute bottom-0 left-1/2 ml-[-639px] w-[1278px] max-w-none"
           priority={true}
         />
+
         <div className="absolute inset-0 shadow-[inset_0_-1px_0_rgba(22,27,59,0.04)]" />
+
         <div className="container relative">
           <div className="flex justify-center text-center lg:pt-5 lg:pb-7 lg:text-left">
             <div className="flex max-w-[37rem] flex-col py-16 lg:py-12">
               <h1 className="mt-6 text-[1.75rem] font-extrabold leading-9 tracking-tight text-slate-800 md:text-4xl">
                 A Software Engineer from West Java - Indonesia.
               </h1>
+
               <div className="order-first flex items-center justify-center gap-4 text-[0.8125rem] leading-6 text-slate-500 lg:justify-start">
                 <p>
                   <code>{'<Hello.World />'}</code>
@@ -52,10 +55,12 @@ export const Header = () => {
                   <code>{`<Good.${greeting()} />`}</code>
                 </p>
               </div>
+
               <p className="mt-4 text-sm text-slate-500">
                 This website was established not just as a learning tool, but also as a place for me to write and show
                 off my projects, as well as a repository of my past experiences.
               </p>
+
               <div className="mt-8 flex flex-col justify-center gap-4 sm:flex-row lg:justify-start">
                 <Link
                   href="#"
@@ -78,6 +83,7 @@ export const Header = () => {
                 </button>
               </div>
             </div>
+
             <div className="hidden lg:flex lg:flex-auto lg:justify-center">
               <Illustration />
             </div>
