cae9f9ce650fd3c34a9bd937f0850038b7843354
Fahmi Idris <fahmiidris.1607@gmail.com>
2023-07-02 16:39:31 +0700

    feat: add base tailwindcss and clsxm helper

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-import { fontFamily } from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import tailwindcssForms from '@tailwindcss/forms';
 
 import type { Config } from 'tailwindcss';
 
@@ -6,21 +8,26 @@ const tailwindConfig = {
     content: ['./src/**/*.{ts,tsx}'],
     theme: {
         extend: {
-            screens: {
-                '2xl': '1536px',
-            },
-            fontFamily: {
-                sans: ['Inter var', ...fontFamily.sans],
-            },
             container: {
                 center: true,
                 padding: {
                     DEFAULT: '1rem',
                 },
             },
+            colors: {
+                primary: colors.cyan,
+                secondary: colors.slate,
+                tertiary: colors.amber,
+            },
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            screens: {
+                '2xl': '1536px',
+            },
         },
     },
-    plugins: [],
+    plugins: [tailwindcssForms],
 } satisfies Config;
 
 export default tailwindConfig;
