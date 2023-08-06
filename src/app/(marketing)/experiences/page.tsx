486fa086e0d46a3cd7d0721cbd68b4f2aeb3792f
Fahmi Idris <fahmiidris.1607@gmail.com>
2023-08-06 15:46:38 +0700

    fix: bug ui experiences page mobile screen

--- src/app/(marketing)/experiences/page.tsx
+++ src/app/(marketing)/experiences/page.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 
+import Image from 'next/image';
+
+import format from 'date-fns/format';
+
+import Button from '@/components/button';
 import Jumbotron from '@/app/(marketing)/experiences/_partials/jumbotron';
 import SearchAndSort from '@/app/(marketing)/experiences/_partials/search-and-sort';
 
 import mdx from '@/utils/mdx';
 import clsxm from '@/utils/clsxm';
-import format from 'date-fns/format';
-import Button from '@/components/button';
 
 export default async function ExperiencesPage() {
     const experiences = await mdx('marketing', 'experiences');
@@ -19,8 +22,8 @@ export default async function ExperiencesPage() {
             <div className="container relative pb-20 pt-10">
                 <ol role="list" className="relative">
                     {experiences.map((experience, index) => {
-                        const startDate = format(new Date(experience.date.start), 'MMMM, yyyy');
-                        const endDate = experience.date.end ? format(new Date(experience.date.end), 'MMMM, yyyy') : 'Present';
+                        const startDate = format(new Date(experience.date.start), 'MMMM yyyy');
+                        const endDate = experience.date.end ? format(new Date(experience.date.end), 'MMMM yyyy') : 'Present';
 
                         const dateTime = `${startDate} - ${endDate}`;
 
@@ -28,7 +31,7 @@ export default async function ExperiencesPage() {
                             <li
                                 key={experience.slug}
                                 className={clsxm(
-                                    `relative grid-cols-1 gap-8 pl-10 before:absolute before:left-0 before:flex before:h-[calc(1.375rem+1px)] before:w-[calc(1.375rem+1px)] before:items-center before:justify-center before:text-[0.625rem] before:font-bold before:text-slate-800 before:ring-1 before:ring-slate-200 before:content-[''] xl:grid`,
+                                    `relative grid grid-cols-1 gap-8 pl-10 before:absolute before:left-0 before:flex before:h-[calc(1.375rem+1px)] before:w-[calc(1.375rem+1px)] before:items-center before:justify-center before:text-[0.625rem] before:font-bold before:text-slate-800 before:ring-1 before:ring-slate-200 before:content-['']`,
                                     index !== experiences.length - 1 &&
                                         'pb-8 after:absolute after:bottom-0 after:left-[0.6875rem] after:top-6 after:w-px after:bg-slate-200'
                                 )}
@@ -40,12 +43,19 @@ export default async function ExperiencesPage() {
                                         {dateTime}
                                     </time>
 
-                                    <p className="line-clamp-2 max-w-4xl text-sm/6 text-slate-700">{experience.description}</p>
+                                    <p className="line-clamp-2 max-w-3xl pt-4 text-sm/6 text-slate-700">{experience.description}</p>
                                 </div>
 
                                 <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
-                                    {[0, 1, 2].map((image) => (
-                                        <div key={image} className="aspect-[16/9] w-full bg-slate-100 object-cover" />
+                                    {experience.images.map((image, index) => (
+                                        <React.Fragment key={index}>
+                                            <Image
+                                                {...image}
+                                                alt=""
+                                                placeholder="blur"
+                                                className="aspect-[16/9] w-full bg-slate-100 object-cover object-center"
+                                            />
+                                        </React.Fragment>
                                     ))}
                                 </div>
 
